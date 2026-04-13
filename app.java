d478aa374b211b8f10b372249fceaa9c5f1b4c40
alvinkbabu <alvin.kbabu2023@vitstudent.ac.in>
2026-04-13 12:12:41 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,9 +1,11 @@
 public class App {
- public static void main(String[] args) throws Exception {
- System.out.println("Java CI/CD Application Started...");
- while (true) {
- Thread.sleep(5000);
- System.out.println("Hello from Java CI/CD Pipeline!");
- }
- }
+    public static void main(String[] args) {
+        int number = 29;
+        System.out.println(number + (isPrime(number) ? " is prime" : " is not prime"));
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) if (n % i == 0) return false;
+        return true;
+    }
 }
